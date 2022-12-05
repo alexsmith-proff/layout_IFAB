5f7aeace12f2fc864c761f462523e5d7308973b4
alexsmith_proff <alexsmith_proff@mail.ru>
2022-12-06 01:06:53 +0300

    add MainFeedback section

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
 
-    $('.menu-burger').click(function(event) {
+	$('.menu-burger').click(function (event) {
 		$(this).toggleClass('open-menu');
 		$('.topnav').toggleClass('responsive');
 	});
+
+	$('.main-feedback__slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		fade: true,
+		speed: 4500,
+		autoplay: true
+	})
 
 
 });
