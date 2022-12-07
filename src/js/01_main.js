9ef0e088288e815b96cd988a070c73acec02b7aa
alexsmith_proff <alexsmith_proff@mail.ru>
2022-12-07 17:16:42 +0300

    add animation to index.html

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -14,6 +14,18 @@ $(document).ready(function () {
 		speed: 4500,
 		autoplay: true
 	})
+	$(window).scroll(() => {
+		let height = $(window).scrollTop();
+		$('.scroll-top').text(height)
+		if(height > 800) {
+			$('.geography__right-item').addClass('animate__animated animate__fadeInUp visible')
+			$('.geography__left').addClass('animate__animated animate__fadeInLeft visible')
+		}
+		if(height > 4000) {
+			$('.contacts__right').addClass('animate__animated animate__fadeInUp visible')
+		}
+	})
+
 
 
 });
